48c991276ee85800456f0f62f8a9165d0d3126c3
Kristian Gotfredsen <ksg2001@live.com>
2024-01-28 13:50:46 +0100

    Now remote_values.h should be in git

---
+++ remote_values.h
@@ -0,0 +1,44 @@
+/*
+
+Here we define the macros for all the buttons. 
+They are indexed like they would be in a matrix
+meaning:
+
+IR_REMOTE_row_column
+
+*/
+
+/*1st row*/
+#define IR_REMOTE_1_1 0xFFA25D
+#define IR_REMOTE_1_2 0xFF629D
+#define IR_REMOTE_1_3 0xFFE21D
+
+/*2nd row*/
+#define IR_REMOTE_2_1 0xFF22DD
+#define IR_REMOTE_2_2 0xFF02FD
+#define IR_REMOTE_2_3 0xFFC23D
+
+/*3rd row*/
+#define IR_REMOTE_3_1 0xFFE01F
+#define IR_REMOTE_3_2 0xFFA857
+#define IR_REMOTE_3_3 0xFF906F
+
+/*4th row*/
+#define IR_REMOTE_4_1 0xFF6897
+#define IR_REMOTE_4_2 0xFF9867
+#define IR_REMOTE_4_3 0xFFB04F
+
+/*5th row*/
+#define IR_REMOTE_5_1 0xFF30CF
+#define IR_REMOTE_5_2 0xFF18E7
+#define IR_REMOTE_5_3 0xFF7A85
+
+/*6th row*/
+#define IR_REMOTE_6_1 0xFF10EF
+#define IR_REMOTE_6_2 0xFF38C7
+#define IR_REMOTE_6_3 0xFF5AA5
+
+/*7th row*/
+#define IR_REMOTE_7_1 0xFF42BD
+#define IR_REMOTE_7_2 0xFF4AB5
+#define IR_REMOTE_7_3 0xFF52AD
